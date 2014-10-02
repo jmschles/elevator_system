a3859bff52c1d3d33e221dc6767b6885718bb64d
Jamie Schlessinger <james.schlessinger@gmail.com>
2014-10-02 08:30:27 -0700

    Write some elevator system functionality

--- elevator.rb
+++ elevator.rb
@@ -1,9 +1,88 @@
 class Elevator
-  attr_reader :id, :floor, :direction
+  attr_reader   :current_floor, :id, :direction
+  attr_accessor :destination_queue
 
   def initialize(id)
-    @id        = id
-    @floor     = 1
-    @direction = nil
+    @id                = id
+    @current_floor     = 1
+    @destination_queue = []
+    @direction         = nil
+  end
+
+  def add_floor_to_destination_queue(destination_floor)
+    unless valid_destination(destination_floor)
+      warn "Cannot go to floor #{destination_floor}: elevator moving wrong way"
+      return
+    end
+    @destination_queue.push(destination_floor)
+    @destination_queue.sort!
+    @destination_queue.reverse! if going_down?
+  end
+
+  def eligible_for_pickup?(requested_direction, requested_floor)
+    return true unless in_use?
+    return false unless @direction == requested_direction
+    return true if moving_in_right_direction?(requested_floor)
+    false
+  end
+
+  def in_use?
+    !!@direction
+  end
+
+  def perform_moves
+    @direction = determine_direction(next_destination)
+    stop_at_current_floor unless @direction
+    move_to_next_destination
+    if @destination_queue.empty?
+      @direction = nil
+    else
+      perform_moves
+    end
+  end
+
+  private
+
+  def determine_direction(destination_floor)
+    return nil if @current_floor == destination_floor
+    (@current_floor < destination_floor) ? :up : :down
+  end
+
+  def going_down?
+    @direction == :down
+  end
+
+  def going_up?
+    @direction == :up
+  end
+
+  def move_one_floor
+    Thread.new do
+      sleep 1.5
+      @direction == :up ? @current_floor += 1 : @current_floor -= 1
+    end
+  end
+
+  def move_to_next_destination
+    move_one_floor until @current_floor == next_destination
+    stop_at_current_floor
+  end
+
+  def moving_in_right_direction?(requested_floor)
+    (going_up? && requested_floor > @current_floor) || (going_down? && requested_floor < @current_floor)
+  end
+
+  def next_destination
+    @destination_queue.first
+  end
+
+  def stop_at_current_floor
+    puts "Ding! Elevator #{@id} arriving at floor #{@destination_queue.last}"
+    @destination_queue.pop
+  end
+
+  def valid_destination?(requested_floor)
+    return true unless in_use?
+    moving_in_right_direction?(requested_floor)
   end
 end
--- elevator_system.rb
+++ elevator_system.rb
@@ -3,8 +3,42 @@ require './elevator.rb'
 class ElevatorSystem
   attr_reader :elevators, :floors
 
-  def initialize(elevators, floors)
-    @elevators = (1..elevators).map { |id| Elevator.new(id) }
+  def initialize(num_elevators, floors)
+    @elevators = (1..num_elevators).map { |id| Elevator.new(id) }
     @floors    = floors
+  end
+
+  # Called when someone requests an elevator
+  def receive_call(requested_direction, requested_floor)
+    puts "Call received: floor #{requested_floor}, going #{requested_direction}"
+    elevator = closest_eligible_elevator(requested_direction, requested_floor)
+    elevator.add_floor_to_destination_queue(requested_floor)
+    elevator.perform_moves unless elevator.in_use?
+  end
+
+  # Called when someone pushes a button inside an elevator
+  def receive_internal_request(elevator_id, requested_floor)
+    puts "Internal request received on Elevator #{elevator_id}: go to floor #{requested_floor}"
+    elevator = @elevators.detect { |e| e.id == elevator_id }
+    elevator.add_floor_to_destination_queue(requested_floor)
+    elevator.perform_moves unless elevator.in_use?
+  end
+
+  private
+
+  def elevators_sorted_by_call_proximity(requested_floor)
+    @elevators.sort_by { |e| (e.current_floor - requested_floor).abs }
+  end
+
+  def closest_eligible_elevator(requested_direction, destination_floor)
+    best_elevator = elevators_sorted_by_call_proximity.detect do |e|
+      e.eligible_for_pickup?(requested_direction, destination_floor)
+    end
+    if best_elevator
+      best_elevator
+    else
+      # sleep here?
+      closest_eligible_elevator(requested_direction, destination_floor)
+    end
   end
 end
